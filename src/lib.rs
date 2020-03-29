6ce50da663a9175ebbac1e59af666def460aac47
James Simpson <james.simpson9339@gmail.com>
2020-03-29 19:23:42 -0400

    feat: Add config

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+pub use config::Config;
 pub use query::Query;
 pub use remote::Remote;
 pub use repository::Repository;
 pub use scp::ScpPath;
 pub use tag::Tag;
 
+pub mod util;
+
+pub mod config;
 mod query;
 mod remote;
 mod repository;
