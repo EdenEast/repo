43aa708539127f3b38ce4220ee051ecd36280941
James Simpson <james.simpson9339@gmail.com>
2020-04-10 14:19:52 -0400

    feat: Filter update command by tags
    Update command can now filter repositories that match a list of tags
    passed in by the user

--- src/bin/repo/ops/update.rs
+++ src/bin/repo/ops/update.rs
@@ -1,11 +1,12 @@
 use super::CliCommand;
 use anyhow::Result;
-use clap::{App, Arg, ArgMatches};
+use clap::{values_t, App, Arg, ArgMatches};
 use repo::prelude::*;
 
 pub struct UpdateCommand {
     local: bool,
     global: bool,
+    tags: Option<Vec<String>>,
 }
 
 impl CliCommand for UpdateCommand {
@@ -23,19 +24,29 @@ impl CliCommand for UpdateCommand {
                     .long("global")
                     .short("g"),
             )
+            .arg(
+                Arg::with_name("tag")
+                    .help("Perform operation on only repositories that contain tag")
+                    .long("tag")
+                    .short("t")
+                    .takes_value(true)
+                    .multiple(true)
+                    .number_of_values(1),
+            )
     }
 
     fn from_matches(m: &ArgMatches) -> Self {
         Self {
             local: m.is_present("local"),
             global: m.is_present("global"),
+            tags: values_t!(m, "tag", String).ok(),
         }
     }
 
     fn run(self, _: &ArgMatches) -> Result<()> {
         let workspace = Workspace::new()?;
 
-        let repositories = match (self.global, self.local) {
+        let mut repositories = match (self.global, self.local) {
             (true, false) => workspace
                 .cache()
                 .repositories()
@@ -50,6 +61,13 @@ impl CliCommand for UpdateCommand {
                 .collect(),
             _ => workspace.cache().repositories(),
         };
+
+        if let Some(tags) = self.tags {
+            repositories = repositories
+                .into_iter()
+                .filter(|r| tags.iter().any(|t| r.tags.contains(t)))
+                .collect::<Vec<&Repository>>();
+        }
 
         for repository in repositories {
             workspace.update_remotes(&repository)?;
