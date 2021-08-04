a839213ebc0a5f929fe42aa522aa33bffbe64fc7
James Simpson <edenofest@gmail.com>
2021-08-04 10:44:59 -0400

    style: nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # Example resources:
 #   - https://github.com/Stupremee/nix/blob/bc76927/templates/rust/flake.nix
 {
-  description = "locations - find your friends";
+  description = "Repo - Repository management utility";
   inputs = {
     utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nmattia/naersk";
@@ -12,38 +12,41 @@
   };
 
   outputs = { self, nixpkgs, utils, naersk, flake-compat }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        manifest = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-        version = manifest.package.version;
-        pname = "repo";
+    utils.lib.eachDefaultSystem (
+      system:
+        let
+          manifest = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+          version = manifest.package.version;
+          pname = "repo";
 
-        pkgs = nixpkgs.legacyPackages."${system}";
+          pkgs = nixpkgs.legacyPackages."${system}";
 
-        naersk-lib = naersk.lib."${system}";
-      in rec {
-        # `nix build`
-        defaultPackage = packages."${pname}";
-        packages."${pname}" = naersk-lib.buildPackage {
-          inherit pname version;
-          root = ./.;
-          copyBins = true;
-          copylibs = false;
-          release = true;
+          naersk-lib = naersk.lib."${system}";
+        in
+          rec {
+            # `nix build`
+            defaultPackage = packages."${pname}";
+            packages."${pname}" = naersk-lib.buildPackage {
+              inherit pname version;
+              root = ./.;
+              copyBins = true;
+              copylibs = false;
+              release = true;
 
-          nativeBuildInputs = [ pkgs.pkg-config ];
-          buildInputs = [ pkgs.openssl ];
-        };
+              nativeBuildInputs = [ pkgs.pkg-config ];
+              buildInputs = [ pkgs.openssl ];
+            };
 
-        # `nix run`
-        apps."${pname}" = utils.lib.mkApp { drv = packages."${pname}"; };
-        defaultApp = apps."${pname}";
+            # `nix run`
+            apps."${pname}" = utils.lib.mkApp { drv = packages."${pname}"; };
+            defaultApp = apps."${pname}";
 
-        # `nix develop`
-        devShell = pkgs.mkShell {
-          name = "${pname}";
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
-          buildInputs = with pkgs; [ openssl pkg-config ];
-        };
-      });
+            # `nix develop`
+            devShell = pkgs.mkShell {
+              name = "${pname}";
+              nativeBuildInputs = with pkgs; [ rustc cargo ];
+              buildInputs = with pkgs; [ openssl pkg-config ];
+            };
+          }
+    );
 }
